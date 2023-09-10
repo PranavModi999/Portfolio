8dbecc38b1b9424e4248a93abce681a8430b2754
PranavModi999 <pranavmodi999@gmail.com>
2023-09-10 16:15:12 -0400

    disigned desktop nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import NauticalWheel from "./ship-wheel.png";
+import Button from "../../UI/Button/Button";
 
 import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
     <nav>
-      <div>
-        <img src={NauticalWheel} alt="Nav Nautical wheel" />
+      <div></div>
+      <ul className={styles.items_container}>
+        <li className={styles.items}>PROFILE</li>
+        <li className={styles.items}>EXPERIENCE</li>
+        <li className={styles.items}>PROJECTS</li>
+      </ul>
+      <div className={styles.contact_container}>
+        <Button>CONTACT</Button>
       </div>
     </nav>
   );

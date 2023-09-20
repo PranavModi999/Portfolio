06fb14c9f72e2de73c784884e3cc99f75666985e
PranavModi999 <pranavmodi999@gmail.com>
2023-09-20 13:10:25 -0400

    started reworking on education componenet

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import brainyBeam from "./brainyBeam.jpeg";
-import conestoga from "./conestoga.jpg";
-import itm from "./itm.jpg";
-import shiraz from "./shiraz.jpg";
-import smartNode from "./smartnode.jpg";
 
 import styles from "./Education.module.css";
 
 const Education = () => {
   return (
-    <div className={styles.staggered_grid}>
-      <div className={styles.grid_itm}>
-        <img src={itm} alt="itm" />
-      </div>
-      <div className={styles.grid_conestoga}>
-        <img src={conestoga} alt="conestoga" />
-      </div>
-
-      <div className={styles.grid_smartNode}>
-        <img src={smartNode} alt="smartNode" />
-      </div>
-      <div className={styles.grid_shiraz}>
-        <img src={shiraz} alt="shiraz" />
-      </div>
-      <div className={styles.grid_brainyBeam}>
-        <img src={brainyBeam} alt="brainyBeam" />
-      </div>
+    <div className={styles.education_container}>
+      <section className={styles.left_container}>
+        <h1>
+          Know Who <span className={styles.emphasis_text}>I'M</span>
+        </h1>
+        <p>
+          I started my journey by enrolling in Bachelor of Engineering in
+          Computer Science at ITM(SLS) Baroda University, Vadodara, Gujarat,
+          from June 2018 to May 2022.During my college life i participated in
+          college offered internship as a Research Intern where I conducted data
+          analysis on sleep disorder data. Furthermore, i took upon another
+          internship opprtunity to learn deeply about object oreinted
+          proggraming at BrainyBeam Pvt Ltd.
+        </p>
+        <p>
+          My most recent professional experience was as a Flutter Developer
+          Intern at SmartNode Pvt. Ltd. During this internship, I independently
+          managed front-end development for Android applications, implementing
+          voice integration, Geo-fencing, and NFC modules.
+        </p>
+        <p>
+          Currently, I am pursuing a Post Graduate Web Development program at
+          Conestoga College in Kitchener, Ontario, where I maintain a GPA of
+          3.88/4.00. My career journey reflects my continuous dedication to
+          software development, data analysis, and technology-driven
+          problem-solving.
+        </p>
+      </section>
+      <section className={styles.right_container}></section>
     </div>
   );
 };

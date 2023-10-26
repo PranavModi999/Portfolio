2d64894fe1e3abd63f1246c34711c6e2c3aa8647
Pranav Modi <pranavmodi@pop-os.localdomain>
2023-10-25 23:15:38 -0400

    added project details

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import budget from "./budget.png";
+import drivetest from "./drivetest.png";
+import expense from "./expense.png";
+import foodOrder from "./foodOrder.png";
 import litHaven from "./litHaven.png";
+import pingPong from "./pingPong.png";
+import skyCrest from "./skycrest.png";
+
 import ProjectCard from "./project_card/ProjectCard";
 
 import styles from "./Project.module.css";
@@ -10,14 +17,41 @@ const Project = React.forwardRef((_, ref) => {
       <h1 className={styles.title}>My Projects</h1>
 
       <div className={styles.project_container}>
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
-        <ProjectCard img={litHaven} />
+        <ProjectCard
+          img={litHaven}
+          demo="https://pranavmodi999.github.io/LitHaven/"
+          link="https://github.com/PranavModi999/LitHaven"
+        />
+        <ProjectCard
+          img={drivetest}
+          demo="N/A"
+          link="https://github.com/PranavModi999/DriveTest"
+        />
+        <ProjectCard
+          img={budget}
+          demo="https://pranavmodi999.github.io/Expense-App/"
+          link="https://github.com/PranavModi999/Expense-App"
+        />
+        <ProjectCard
+          img={foodOrder}
+          demo="https://pranavmodi999.github.io/Food-Order-App/"
+          link="https://github.com/PranavModi999/Food-Order-App"
+        />
+        <ProjectCard
+          img={skyCrest}
+          demo="https://pranavmodi999.github.io/SkyCrest-Airline/"
+          link="https://github.com/PranavModi999/SkyCrest-Airline"
+        />
+        <ProjectCard
+          img={expense}
+          demo="N/A"
+          link="https://github.com/PranavModi999/budget-app"
+        />
+        <ProjectCard
+          img={pingPong}
+          demo="N/A"
+          link="https://github.com/PranavModi999/PingPong-Game"
+        />
       </div>
     </div>
   );
--- src/components/project/project_card/ProjectCard.jsx
+++ src/components/project/project_card/ProjectCard.jsx
@@ -2,13 +2,27 @@ import React from "react";
 
 import styles from "./ProjectCard.module.css";
 
-const ProjectCard = ({ img, children }) => {
+const ProjectCard = ({ img, demo, link }) => {
   return (
     <div className={styles.project}>
       <img src={img} alt="lithaven" />
       <div className={styles.links}>
-        <p>Demo</p>
-        <p>Code</p>
+        <p>
+          <a
+            className={demo === "N/A" ? styles.linksCross : styles.linksHover}
+            href={demo}
+          >
+            Demo
+          </a>
+        </p>
+        <p>
+          <a
+            className={link === "N/A" ? styles.linksCross : styles.linksHover}
+            href={link}
+          >
+            Link
+          </a>
+        </p>
       </div>
     </div>
   );
